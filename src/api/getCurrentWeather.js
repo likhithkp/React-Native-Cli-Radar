9f080a26cb3f1f04cc6b685efdfcac6ad49faab6
likhithkp <likhithkpdev@gmail.com>
2024-05-08 10:29:41 +0530

    API changes

--- src/api/getCurrentWeather.js
+++ src/api/getCurrentWeather.js
@@ -1,7 +1,12 @@
 export const getWeather = async () => {
-  const result = await fetch(
-    'https://api.openweathermap.org/data/2.5/weather?q=madikeri&appid=13cffaf83ca230a33e17245b5aa557f8&units=metric',
-  );
-  const data = await result.json();
-  return data;
+  try {
+    const result = await fetch(
+      'https://api.openweathermap.org/data/2.5/weather?q=madikeri&appid=13cffaf83ca230a33e17245b5aa557f8&units=metric',
+    );
+    const data = await result.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching weather data:', error);
+    throw error;
+  }
 };
