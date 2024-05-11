9ab87aa269c28aac16e1cf9199f64865a3c524d1
likhithkp <likhithkpdev@gmail.com>
2024-05-11 20:10:46 +0530

    FlatList data component created

---
+++ src/screens/FlatlistDataScreen.js
@@ -0,0 +1,10 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+export default function FlatListData() {
+  return (
+    <View>
+      <Text>FlatListDataScreen</Text>
+    </View>
+  );
+}
